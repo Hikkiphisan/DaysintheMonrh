24fa37c6a51fe3fcdd641c08eebec4a065b8314d
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-10-10 14:53:25 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int month = 0;
+        while (true) {
+            month = scanner.nextInt();
+            if (month >= 1 && month <= 12) {
+                break;
+            } else {
+                System.out.println("Gia tri khong hop le");
+            }
+        }
+        int daysinMonth;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+              daysinMonth = 31;
+             break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysinMonth = 30;
+                break;
+            case 2:
+                daysinMonth = 28; // Giả định không phải năm nhuận
+                break;
+            default:
+                daysinMonth = 0;
+        }
+        System.out.println("Tháng " + month + " có " + daysinMonth + " ngày.");
+    }
+}
